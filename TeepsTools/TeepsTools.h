3a5a2f7efe6b912da587f19bdbaeb5e82dac31b4
cjbarth1 <cjbarth1@gmail.com>
2017-07-24 13:08:27 -0400

    Initial Commit

---
+++ TeepsTools/TeepsTools.h
@@ -0,0 +1,19 @@
+//
+//  TeepsTools.h
+//  TeepsTools
+//
+//  Created by Casey Barth on 7/24/17.
+//  Copyright © 2017 teeps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TeepsTools.
+FOUNDATION_EXPORT double TeepsToolsVersionNumber;
+
+//! Project version string for TeepsTools.
+FOUNDATION_EXPORT const unsigned char TeepsToolsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TeepsTools/PublicHeader.h>
+
+
