748652b593827364cfe9e185e1d71e98e3f74b6e
Casey Barth <cjbarth1@gmail.com>
2017-09-19 15:55:33 -0400

    Add image picker protocol

---
+++ TeepsTools/ImagePicking.h
@@ -0,0 +1,13 @@
+//
+//  ImagePicking.h
+//  TeepsTools
+//
+//  Created by Casey Barth on 9/19/17.
+//  Copyright © 2017 teeps. All rights reserved.
+//
+
+#ifndef ImagePicking_h
+#define ImagePicking_h
+
+
+#endif /* ImagePicking_h */
