89ccccc11df808c50887a790c7e952b8959c276c
Casey Barth <cjbarth1@gmail.com>
2017-09-19 14:41:41 -0400

    Add alerting protocol

---
+++ TeepsTools/Alerting.swift
@@ -0,0 +1,38 @@
+//
+//  Alerting.swift
+//  TeepsTools
+//
+//  Created by Casey Barth on 9/19/17.
+//  Inspired by James Smith
+//  Copyright © 2017 teeps. All rights reserved.
+//
+
+import UIKit
+
+protocol Alerting {
+  func showError(withTitle title: String, description: String)
+}
+
+// MARK: - Errors
+extension Alerting where Self: UIViewController {
+  func showError(withTitle title: String, description: String) {
+    let alert = UIAlertController(title: title, message: description, preferredStyle: .alert)
+    self.present(alert, animated: true, completion: nil)
+  }
+  
+  /// Show an error with the default title
+  func showError(withDescription description: String) {
+    showError(withTitle: "Sorry!", description: description)
+  }
+  
+  /// Show an error displaying the reason it occurred
+  func showError(_ error: NSError) {
+    let description = error.localizedFailureReason ?? error.localizedDescription
+    showError(withDescription: description)
+  }
+  
+  /// Show an error displaying the reason it occurred
+  func showError(_ error: Error) {
+    self.showError((error as Any) as! NSError)
+  }
+}
