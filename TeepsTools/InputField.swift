230b702d0e48e0828e4edc8e5b3dbfbfca3b6700
Casey Barth <cjbarth1@gmail.com>
2017-09-20 11:09:50 -0400

    Make convenience inits for inputfield

--- TeepsTools/InputField.swift
+++ TeepsTools/InputField.swift
@@ -6,7 +6,31 @@
 //  Copyright © 2017 teeps. All rights reserved.
 //
 
-class InputField: UITextField, Validating {
+/** USAGE:
+ Storyboard: 
+ Set custom class of a TextField to InputField.
+ Create an outlet in your view.
+ Set the fieldType - inputField.fieldType = .email
+ 
+ Programatically:
+ Using prebuilt field types - let inputField = InputField(fieldType: .email)
+ Using custom regex - let inputField = InputField(customRegex: "[0-9a-fxA-FX]")
+ 
+ Validate input matches Regex by calling inputField.validates
+*/
+ 
+
+public class InputField: UITextField, Validating {
   var fieldType: RegexType?
   var customRegex: String?
+  
+  convenience init(fieldType: RegexType) {
+    self.init()
+    self.fieldType = fieldType
+  }
+  
+  convenience init(customRegex: String) {
+    self.init()
+    self.customRegex = customRegex
+  }
 }
